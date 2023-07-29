cd3a2e389d3f53287b5efcd33685a97539770778
karshoul <khuongmingo@gmail.com>
2023-07-29 20:04:20 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CS_hello_world
+{
+    class Program
+    {
+        /*
+            Đây là hàm Main
+            Điểm chạy chương trình
+        */
+        static void Main(string[] args)
+        {
+            // Đây là nội dung in ra màn hình
+            Console.WriteLine("Xin chao C#!");
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Phương thức trả về tổng của hai số nguyên
+        /// </summary>
+        /// <param name="a"> số nguyên 1 </param>
+        /// <param name="b"> số nguyên 2 </param>
+        /// <returns> Tổng a + b </returns>
+        static int Tong(int a, int b) 
+        {
+            return ( a + b );
+        }
+    }
+}
