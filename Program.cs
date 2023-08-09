cf78d359de68971e66be6a43fcc0f88c551c3127
karshoul <khuongmingo@gmail.com>
2023-08-09 20:18:35 +0700

    String characters in C Sharp

--- Program.cs
+++ Program.cs
@@ -1,110 +1,64 @@
-﻿// using System;
-// using System.Linq;
-
-// namespace CS_hello_world
-// {
-//     class Program
-//     {
-//         /*
-//             Đây là hàm Main
-//             Điểm chạy chương trình
-//         */
-//         class Student {
-//             public string name;
-//             public Student(string name) {
-//                 this.name = name;
-//                 Console.WriteLine("Khoi tao " + name);
-//             }
-
-//             ~Student()
-//             {
-//                 Console.ForegroundColor = ConsoleColor.Red;
-//                 Console.WriteLine("Huy " + name);
-//                 Console.ResetColor();
-//             }
-
-//         }
-//         static void Main(string[] args)
-//         {   
-//         //    Vukhi sungluc; // null
-//         //    sungluc = new Vukhi();
-//         //    sungluc.name = "Sung luc";
-//         //    sungluc.Thietlapdosatthuong(5);
-           
-//         //    sungluc.Noisanxuat = "My";
-//         //    Console.WriteLine(sungluc.Noisanxuat);
-
-//         //    Vukhi sungmay = new Vukhi("Sung may",15);
-
-//         //    sungluc.Tancong();
-//         //    sungmay.Tancong();
-
-//         // HÀM HỦY
-//         Student student;
-        
-//         for (int i = 0; i < 1000; i++)
-//         {
-//             student = new Student("Sinh vien " + i);
-
-//             student = null;
-//         }
-
-//         GC.Collect();
-
-//         } 
-//     }
-// }
-
-using System;
+﻿using System;
 using System.Linq;
 
 namespace CS_hello_world
 {
-    /*
-        Đây là hàm Main
-        Điểm chạy chương trình
-    */
-    class Student : IDisposable
-    {
-        public string name;
-        public Student(string name)
-        {
-            this.name = name;
-            Console.WriteLine("Khoi tao " + name);
-        }
-
-        ~Student()
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Huy " + name);
-            Console.ResetColor();
-        }
-
-        public void Dispose()
-        {
-            // Giải phóng tài nguyên mà nó chiếm giữ
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Huy (boi dispose) " + name);
-            Console.ResetColor();
-        }
-    }
-
     class Program
     {
-        static void test()
-        {
-            using Student sv = new Student("Ten");
-            Console.WriteLine("1");
-            Console.WriteLine("2");
-            Console.WriteLine("3"); 
-        }
+        /*
+            Đây là hàm Main
+            Điểm chạy chương trình
+        */
         static void Main(string[] args)
         {
-            // using (Student s = new Student("Ten sinh  vien"))
-            {
-                test();
-                // biến s chỉ có hiệu lực trong này
-            }
+            //  string loichao; //null
+            //  string ten = "Khuong Nguyen";
+            //  loichao = "Xin chao";
+
+            //  string thongbao = loichao + " " + ten; 
+            //  thongbao += "!";
+            //  Console.WriteLine(thongbao); 
+            
+            // string thongbao;
+            // thongbao = "\\Hoc\t ve\t chuoi ki tu\n \"String\"";
+            // // Để chèn kí tự dấu nháy kép: \"
+            // // Để chèn dấu chéo ngược: \\
+            // // Để chèn dấu tab: \t
+            // // Để chèn ký tự xuống dòng: \n
+            // // Để trở về đầu dòng: \r
+            // Console.WriteLine(thongbao);
+
+
+            //có dầu @ có thể xuống dòng, muốn chèn dấu nháy kép: "" tương đương 1 dấu "
+            // string thongbao;
+            // thongbao = @"Xin chao       ""2021"" \
+            
+            // Hoc lap trinh C#
+            // ";
+
+            // Console.WriteLine(thongbao);
+
+            // CHÈN GIÁ TRỊ, BIỂU THỨC GIÁ TRỊ VÀO CHUỖI STRING
+            // string thongbao;
+            // int year = 2020;
+            // thongbao = 
+            // $"Xin chao {year, -10}, nam sau la nam {year + 1}"; 
+            // // {year, 10} 10 là chèn chỗ trống 10 kí tự trước biểu thức (căn lề phải)
+            // // căn lề trái thêm dấu trừ vào chỗ kí tự VD: {year. -10}
+            // Console.WriteLine(thongbao);
+
+            string thongbao;
+            string name = "Khuong";
+            int year = 2005;
+            string gioitinh = "nam";
+            thongbao = 
+            $@"
+            Ho ten: {name, 10}
+            Nam sinh: {year, 10}
+            Gioi tinh: {gioitinh, 10}
+            ";
+
+            Console.WriteLine(thongbao);
         }
     }
 }
