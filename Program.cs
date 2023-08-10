90fa6c34e4cf0033227492346b8f158d48a15b6f
karshoul <khuongmingo@gmail.com>
2023-08-10 20:51:18 +0700

    foreach

--- Program.cs
+++ Program.cs
@@ -47,18 +47,40 @@ namespace CS_hello_world
             // // căn lề trái thêm dấu trừ vào chỗ kí tự VD: {year. -10}
             // Console.WriteLine(thongbao);
 
-            string thongbao;
-            string name = "Khuong";
-            int year = 2005;
-            string gioitinh = "nam";
-            thongbao = 
-            $@"
-            Ho ten: {name, 10}
-            Nam sinh: {year, 10}
-            Gioi tinh: {gioitinh, 10}
-            ";
+            // VD
+            // string thongbao;
+            // string name = "Khuong";
+            // int year = 2005;
+            // string gioitinh = "nam";
+            // thongbao = 
+            // $@"
+            // Ho ten: {name, 10}
+            // Nam sinh: {year, 10}
+            // Gioi tinh: {gioitinh, 10}
+            // ";
+
+            // Console.WriteLine(thongbao);
+
+            string thongbao = "Khuong Nguyen, xin chao cac ban!";
+            int dodai = thongbao.Length; // Đếm bao nhiêu ký tự
+            //Để đọc một ký tự nào đó trong chuỗi ta sử dụng ngoặc vuông
+            // char kytu = thongbao[4];
+
 
             Console.WriteLine(thongbao);
+            // Console.WriteLine(dodai);
+            // Console.WriteLine(kytu);
+
+            // for (int i = 0; i < dodai; i++)
+            // {
+            //     char kytu = thongbao[i];
+            //     Console.WriteLine($"Chi so {i} la ky tu: {kytu, 3}");
+            // }
+
+            foreach (var kytu in thongbao)
+            {
+                Console.WriteLine(kytu);
+            }
         }
     }
 }
