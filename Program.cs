73af0ef914dec763a645c4cae63e092590838024
karshoul <khuongmingo@gmail.com>
2023-08-25 20:53:36 +0700

    null and nullable

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks.Dataflow;
 
 namespace CS_hello_world
 {
@@ -8,58 +9,55 @@ namespace CS_hello_world
             Đây là hàm Main
             Điểm chạy chương trình
         */
+        // THAM CHIẾU
+        class Abc {
+            public void Xinchao() => Console.WriteLine("Xin chao C#");
+        }
         static void Main(string[] args)
         {
-            /*
-            Kieu_du_lieu Ten_bien;
-            *Ten_bien: 
-                1. a...z A...Z
-                2. 0...9
-                3. _
-                4. Không bắt đầu bằng số
-            */
-            // double so_pi;
-            // so_pi = 3.14;
+            // null. nullable
+            /* null là từ khóa nó cho biết là nó không tham chiếu đến một đối tượng nào cả, được sử dụng cho biến kiểu tham 
+            chiếu */
 
-            // Console.Clear();
+            // Abc a = new Abc();
+            // // cách 1
+            // if (a != null)
+            //     a.Xinchao();
+            // //Gọi phương thức khi đối tượng khác null
+            // // Cách 2
+            // a?.Xinchao();
 
-            // Console.Title = "Vi du su dung console";
+            // ĐỐI VỚI BIẾN KIỂU THAM TRỊ 
+            // Không được phép gán giá trị null
+            // int i;
+            // i = null;
 
-            // Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            // Console.BackgroundColor = ConsoleColor.Cyan;
+            // -------------NULLABLE
+            /* Để khai báo một biến kiểu tham trị có khả năng nhận giá trị null tha thêm dấu ? 
+            VD:  int? age; */
+            int? age;
+            age = null;
 
-            // Console.WriteLine("Xin chao, chuong trinh nhap xuat du lieu console");
+            age = 110;
+            // Để kiểm tra xem một biến nullable có có giá trị hay không thì ta sử dụng thuộc tính Hasvalue
+            // if (age.HasValue) 
+            // {
+            //     int _age = age.Value;
+            //     Console.WriteLine(_age);
+            // }
 
-            // Console.ResetColor();
+            // Ta có thể kiểm tra giá trị khác null
 
-            // string hovaten;
-            // Console.Write("Ho ten cua ban:");
-            // hovaten = Console.ReadLine();
+            if (age != null) 
+            {
+                /*Khi ta biết chắn chán biến age khác null thì thay vì đọc giá trị của nó bằng thuộc tính Value như trên
+                    thì ta có thể sử dụng kỹ thuật ép kiểu */
 
-            // Console.WriteLine("Xin chao {0}",hovaten);
+                //Ép kiểu như này nghĩa là ta truyền giá trị của biến age này sang kiểu int và gán cho biến tham trị int bth
+                int _age = (int)age;
+                Console.WriteLine(_age);
+            }
 
-            // float a, b;
-            // string sinput;
-
-            // Console.WriteLine("Hay nhap tham so a:");
-            // sinput = Console.ReadLine();
-
-            // // Chuyển kiểu chuỗi sang số
-            // a = float.Parse(sinput);
-
-            // Console.WriteLine("Hay nhap tham so b:");
-            // sinput = Console.ReadLine();
-            // b = Convert.ToSingle(sinput);
-
-            // Console.Write("So a = {0}, b = {1}",a, b);
-            
-            // const: hằng số không thể thay đổi giá trị
-
-            const double pi = 3.14;
-            const string web = "Thutu";
-
-            Console.WriteLine(pi);
-            Console.WriteLine(web);
         }
     }
 }
